4d0c82fdc8797eb70eec23de836aebafe5b60579
estebes <estebes@localhost.localdomain>
2015-11-14 17:16:20 +0000

    Basic IC2 Classic compatibility patch

--- src/main/java/com/estebes/gravisuitereloaded/GraviSuiteReloaded.java
+++ src/main/java/com/estebes/gravisuitereloaded/GraviSuiteReloaded.java
@@ -1,16 +1,19 @@
 package com.estebes.gravisuitereloaded;
 
 import com.estebes.gravisuitereloaded.init.ItemInit;
+import com.estebes.gravisuitereloaded.init.ItemInitClassic;
 import com.estebes.gravisuitereloaded.init.RecipeInit;
+import com.estebes.gravisuitereloaded.init.RecipeInitClassic;
 import com.estebes.gravisuitereloaded.proxy.ServerProxy;
 import com.estebes.gravisuitereloaded.reference.Reference;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, dependencies = "required-after:IC2@(2.2.780,)", version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, dependencies = "required-after:IC2", version = Reference.VERSION)
 public class GraviSuiteReloaded {
     @SidedProxy(clientSide = Reference.PROXY_CLIENT, serverSide = Reference.PROXY_SERVER)
     public static ServerProxy proxy;
@@ -24,11 +27,21 @@ public class GraviSuiteReloaded {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent init) {
-        ItemInit.init();
+        if(Loader.isModLoaded("IC2-Classic-Spmod")) {
+            ItemInitClassic.init();
+        }
+        else {
+            ItemInit.init();
+        }
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent postinit) {
-        RecipeInit.init();
+        if(Loader.isModLoaded("IC2-Classic-Spmod")) {
+            RecipeInitClassic.init();
+        }
+        else {
+            RecipeInit.init();
+        }
     }
 }
