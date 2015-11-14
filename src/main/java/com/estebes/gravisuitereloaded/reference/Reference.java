b8eb4d0a299c7c68ade66d5402a9178fd1b237e6
estebes <vitorandrade.xtbx@gmail.com>
2015-11-14 17:26:25 +0000

    Version number fix

--- src/main/java/com/estebes/gravisuitereloaded/reference/Reference.java
+++ src/main/java/com/estebes/gravisuitereloaded/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
     public static final String MOD_ID = "GraviSuiteReloaded";
     public static final String LOWERCASE_MOD_ID = MOD_ID.toLowerCase();
     public static final String MOD_NAME = "GraviSuite Reloaded";
-    public static final String VERSION = "1.7.10-0.6";
+    public static final String VERSION = "1.7.10-0.7";
     public static final String PROXY_CLIENT = "com.estebes.gravisuitereloaded.proxy.ClientProxy";
     public static final String PROXY_SERVER = "com.estebes.gravisuitereloaded.proxy.ServerProxy";
 
